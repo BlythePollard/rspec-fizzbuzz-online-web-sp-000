b2d2bca3a8b8682993b2e95429cc0580304c2099
Blythe Pollard <blytheepollard@gmail.com>
2019-08-25 22:33:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
 def fizzbuzz
+  if int % 3 == 0
+    put "Fizz"
+  elsif int % 5 == 0  
+    put "Buzz"
 end
+
